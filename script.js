9387192e46902bee24cf739ed23c4dad81dbf6e8
kthames <135296109+kthames@users.noreply.github.com>
2023-07-25 19:18:35 -0700

    Working on Java API calls

---
+++ script.js
@@ -0,0 +1,13 @@
+var fetchURL = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyBE4-QzODzzAapaJHBTHgNAaq2vjRXw8-0&part=snippet,contentDetails,statistics,status`
+
+
+// async function getData() {
+//     let current = await (await fetch(fetchURL)).json();//wait for the data and wait for the json
+
+//     console.log(current);
+
+// }
+
+fetch(fetchURL).then(data => data.json()).then(data => console.log(data));
+
+//getData();
